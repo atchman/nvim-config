335b45853537d40a4e3d5b377b64018d620a24cf
atchman@eos17 <atchman@hotmail.de>
2023-04-02 00:00:37 +0200

    add /lua/atchman/init.lua

---
+++ lua/atchman/init.lua
@@ -0,0 +1,25 @@
+-- main init of atchman
+
+-- install lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Option / Core
+require "atchman.config.options"
+require "atchman.config.keymaps"
+
+
+-- Plugins managed by lazy.nvim
+require("lazy").setup("plugins")
+
