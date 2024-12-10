b3b7f29b2ef5821e36ea98ed9cc295acf02e321e
atchman <atchman@users.noreply.github.com>
2024-12-10 14:43:20 +0100

    ftplugin: little changes for go & yaml

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,4 +1,7 @@
-vim.opt_local.expandtab = false
+vim.o.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.o.softtabstop = 4
 
 vim.keymap.set("n", "<space>td", function()
 	require("dap-go").debug_test()
--- after/ftplugin/yaml.lua
+++ after/ftplugin/yaml.lua
@@ -1,2 +1,7 @@
+-- yaml files
+--
+
+vim.o.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.o.softtabstop = 2
