bd7842518bfa5c02fd8ce972b2afcbfeeb2089d0
atchman <atchman@users.noreply.github.com>
2024-12-18 23:09:52 +0100

    feat(plugin): treesitter.lua

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,38 @@
+-- treesitter.nvim
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "markdown",
+          "markdown_inline"
+        },
+        -- sync_install = false,
+        auto_install = false,
+        -- ignore_install = { "javascript" },
+        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+        highlight = {
+          enable = true,
+          -- large files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+
+          -- regex
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end,
+  }
+}
