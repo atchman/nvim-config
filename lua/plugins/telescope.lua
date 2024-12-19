85b031cf8fc20205f6ba181cfc01055f6d1ee258
atchman <atchman@users.noreply.github.com>
2024-12-19 23:28:48 +0100

    feat: multigrep big thank you to tjdevries videos about advent of neovim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,15 +9,33 @@ return {
     },
     config = function()
       require('telescope').setup {
+        extensions = {
+          fzf = {}
+        }
       }
+
+      require('telescope').load_extension('fzf')
+
       vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
       vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+
+      -- nvim config
       vim.keymap.set("n", "<space>en", function()
         local opts = require('telescope.themes').get_ivy({
           cwd = vim.fn.stdpath("config")
         })
         require('telescope.builtin').find_files(opts)
       end)
+
+      -- -- packages installed with lazy
+      -- vim.keymap.set("n", "<space>ep", function()
+      --   require('telescope.builtin').find_files {
+      --     cmd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+      --   }
+      -- end)
+
+      -- multigrep
+      require "config.multigrep".setup()
     end
   }
 }
