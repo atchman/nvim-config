5f95ac1350c7ce602775d4e0610edac74ddeba52
atchman <atchman@users.noreply.github.com>
2024-12-21 01:18:44 +0100

    feat(plugin): completion add blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,21 +2,20 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
+      'saghen/blink.cmp',
       {
         "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
         opts = {
           library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
             { path = "${3rd}/luv/library", words = { "vim%.uv" } },
           },
         },
       },
     },
     config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
       -- installed lua lsp with system package manager
-      require("lspconfig").lua_ls.setup {}
+      require("lspconfig").lua_ls.setup { capabilities = capabilities }
 
       -- auto formating on key
       -- vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end)
@@ -25,6 +24,7 @@ return {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
           if not client then return end
+
           if client.supports_method('textDocument/formatting') then
             -- Format the current buffer on save
             vim.api.nvim_create_autocmd('BufWritePre', {
