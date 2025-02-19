96ccb522e72810ee6cf1b9f57fbcafbdb1cecc70
atchman <atchman@users.noreply.github.com>
2025-02-19 22:09:19 +0100

    feat: snacks, lazydev, ... from snacks is dashboard, bigfile, image outsourcing to lazydev.lua

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -9,5 +9,6 @@ return {
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    lazy = false,
   }
 }
