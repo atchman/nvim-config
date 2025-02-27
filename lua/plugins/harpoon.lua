284110222b48b3804f5df06e18af37608b6f80e6
atchman <atchman@users.noreply.github.com>
2025-02-27 20:39:56 +0100

    feat: more plugins gitsigns harpoon trouble undotree, but not active which-key

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,28 @@
+-- harpoon
+--
+return {
+  {
+    "theprimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup({})
+    end,
+    keys = {
+      {
+        "<leader>a",
+        function()
+          require('harpoon'):list():add()
+        end,
+        { desc = "Harpoon [A]dd" }
+      },
+      {
+        "<C-e>",
+        function()
+          require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())
+        end,
+        { desc = "Harpoon List" }
+      },
+    },
+  }
+}
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+-- trouble
+--
+return {
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+  }
+}
