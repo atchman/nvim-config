32e95d13d93de6064846fe5b21b0c83fd774aa73
atchman <atchman@users.noreply.github.com>
2025-03-30 23:41:12 +0200

    fix(lsp): enable lsp
    forgot to commit

--- init.lua
+++ init.lua
@@ -46,27 +46,34 @@ vim.keymap.set("n", "gV", "`[v`]", opts)
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 -- terminal
 vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
 })
 local job_id = 0
 vim.keymap.set("n", "<space>st", function()
-    vim.cmd.vnew()
-    vim.cmd.term()
-    vim.cmd.wincmd("J")
-    vim.api.nvim_win_set_height(0, 5)
-    job_id = vim.bo.channel
-  end,
-  { desc = "[S] [T]erminal" })
+        vim.cmd.vnew()
+        vim.cmd.term()
+        vim.cmd.wincmd("J")
+        vim.api.nvim_win_set_height(0, 5)
+        job_id = vim.bo.channel
+    end,
+    { desc = "[S] [T]erminal" })
+
+-- lsp
+vim.lsp.enable({
+    'lua_ls',
+    'gopls',
+    'rust-analyzer',
+})
