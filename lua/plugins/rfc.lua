f559a680227ad8b32433b2103f5204246c26fa68
atchman <atchman@users.noreply.github.com>
2025-04-16 22:12:03 +0200

    feat(plugin): rfc

---
+++ lua/plugins/rfc.lua
@@ -0,0 +1,18 @@
+-- RFC plugin
+--
+
+return {
+  "exit91/rfc.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "vim-scripts/rfc-syntax",
+  },
+  config = function()
+    require("telescope").load_extension("rfc")
+  end,
+      keys = {
+        { "<leader>sr", function() require('telescope').extensions.rfc.rfc() end, { desc = "RFC Browser" } },
+    },
+
+}
