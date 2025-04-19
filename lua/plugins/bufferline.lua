4e589ee47e438eeac2ebde2731037ca292866075
atchman <atchman@users.noreply.github.com>
2025-04-19 21:14:01 +0200

    fix(plugin): bufferline, treesitter
    merge of bufferline-editor and bufferline
    updated lazy-lock

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,8 +1,30 @@
--- bufferline
+-- bufferline-editor
 --
 return {
-  'akinsho/bufferline.nvim',
-  version = '4.x',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {},
+  {
+    "akinsho/bufferline.nvim",
+    version = '4.x',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
+  {
+    "cornservant/bufferline-editor.nvim",
+    dependencies = {
+      "akinsho/bufferline.nvim"
+    },
+    ---@type bufferline-editor.config
+    opts = {
+      max_width = 50,
+      max_height = 20,
+    },
+    keys = {
+      {
+        "<C-e>",
+        function()
+          require("bufferline-editor").editor_toggle()
+        end,
+        desc = "Bufferline Editor Toogle",
+      },
+    },
+  },
 }
