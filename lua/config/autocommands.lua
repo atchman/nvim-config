a2b576fb352c1448397a6a04d41731726e7eff42
atchman <atchman@users.noreply.github.com>
2025-04-23 23:19:09 +0200

    feat(keymaps): clipboard

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -12,3 +12,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+local job_id = 0
+vim.keymap.set("n", "<space>ct", function()
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 5)
+    job_id = vim.bo.channel
+  end,
+  { desc = "[C]reate [T]erminal" })
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,18 @@
 -- keymaps
 --
 
+-- Escape
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear highlights on search" })
+vim.keymap.set("i", "<C-c>", "<esc>", { desc = "Es[c]ape" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Escape Terminal" })
+-- vim.keymap.set("t", "<leader>q", "<cmd>quit<cr>", { desc = "[Q]uit" })
+
+-- clipboard
+vim.keymap.set("n", "<leader>y", '"+y', { desc = "[y]ank Clipboard" })
+vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "[Y]ank Clipboard" })
+vim.keymap.set("v", "<leader>p", '"+p', { desc = "[p]aste Clipboard" })
+vim.keymap.set("v", "<leader>P", '"+P', { desc = "[P]aste Clipboard" })
+
 -- Source File
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "source file" })
 vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = "source .lua file" })
@@ -14,10 +26,5 @@ vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Qui[C]kfix [P]revious" 
 vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Buffer" })
 vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
 
--- Escape
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear highlights on search" })
-vim.keymap.set("i", "<C-c>", "<esc>", { desc = "Es[c]ape" })
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Escape Terminal" })
-
 -- select last inserted text
-vim.keymap.set("n", "gV", "`[v`]", opts)
+vim.keymap.set("n", "gV", "`[v`]", { desc = "Select last inserted text" })
