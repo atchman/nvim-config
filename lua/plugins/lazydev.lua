5b93b33e49e5fa9dedb1ab13c2f692672325fe48
atchman <atchman@users.noreply.github.com>
2025-06-30 22:56:54 +0200

    fix(lsp): code cleanup

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -2,18 +2,15 @@
 --
 
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-      snippets = {
-        preset = "luasnip",
-      },
+  "folke/lazydev.nvim",
+  ft = "lua", -- only load on lua files
+  ---@class lazydev.Config
+  opts = {
+    ---@type lazydev.Library.spec[]
+    library = {
+      -- See the configuration section for more details
+      -- Load luvit types when the `vim.uv` word is found
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
     },
   },
 }
--- lua/plugins/lsp-server.lua
+++ lua/plugins/lsp-server.lua
@@ -2,73 +2,98 @@
 --
 
 -- LSP Servers
+---@class LspServersConfig
+---@field servers table<string, vim.lsp.Config>
 local servers = {
-  -- ansible
-  "ansible-language-server",
-  "ansible-lint",
-  -- bash
-  "bash-language-server",
+  -- ansible-language-server
+  "ansiblels",
+  -- bash-language-server
+  "bashls",
   -- golang
   "gopls",
-  -- lua
-  "lua-language-server",
-  "stylua", -- Used to format Lua code
+  -- lua-language-server
+  "lua_ls",
   -- lua_ls = {
-  --   settings = {
-  --     Lua = {
-  --       completion = {
-  --         callSnippet = "Replace",
-  --       },
-  --       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-  --       -- diagnostics = { disable = { 'missing-fields' } },
-  --     },
-  --   },
-  -- },
-  -- rust
-  "rust-analyzer",
-  -- shell
-  "shfmt",
-  -- systemd
-  "systemd-language-server",
-  "systemdlint",
-  -- typst
-  "tinymist",
-}
+    --   settings = {
+      --     Lua = {
+        --       completion = {
+          --         callSnippet = "Replace",
+          --       },
+          --       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+          --       -- diagnostics = { disable = { 'missing-fields' } },
+          --     },
+          --   },
+          -- },
+          -- rust-analyzer
+          "rust_analyzer",
+          -- systemd-language-server
+          "systemd_ls",
+          -- typst
+          "tinymist",
+        }
 
--- capabilities
-local capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
--- capabilities
--- local capabilities = {
---   textDocument = {
---     foldingRange = {
---       dynamicRegistration = false,
---       lineFoldingOnly = true
---     }
---   }
--- }
+        -- linter, ...
+        ---@field others table<string, vim.lsp.Config>
+        local others = {
+          -- ansible linter
+          "ansible-lint",
+          -- golang formatter
+          "goimports",
+          "golines",
+          "gomodifytags",
+          "gotests",
+          -- golang linter
+          "golangci-lint",
+          -- shell formatter (sh/bash/mksh)
+          "shfmt",
+          -- lua formatter
+          "stylua", -- Used to format Lua code
+          -- systemd linter
+          "systemdlint",
+        }
 
-return {
-  {
-    "neovim/nvim-lspconfig",
-    enabled = true,
-    version = "2.*",
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    enabled = true,
-    version = "2.*",
-    dependencies = {
-      "neovim/nvim-lspconfig",
-      "mason-org/mason.nvim",
-      "saghen/blink.cmp",
-      "folke/lazydev.nvim",
-    },
-    opts = {
-      -- ensure_installed = ensure_installed,
-      -- automatic_installation = false,
-      ensure_installed = ensure_installed,
-      automatic_enable = true
-    },
-  },
-}
+        -- capabilities
+        -- local capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
+        -- capabilities
+        -- local capabilities = {
+          --   textDocument = {
+            --     foldingRange = {
+              --       dynamicRegistration = false,
+              --       lineFoldingOnly = true
+              --     }
+              --   }
+              -- }
 
+              return {
+                {
+                  "neovim/nvim-lspconfig",
+                  enabled = true,
+                  version = "2.*",
+                },
+                {
+                  "mason-org/mason-lspconfig.nvim",
+                  enabled = true,
+                  version = "2.*",
+                  dependencies = {
+                    "mason-org/mason.nvim",
+                    "neovim/nvim-lspconfig",
+                    "folke/lazydev.nvim",
+                    "saghen/blink.cmp",
+                  },
+                  opts = {
+                    -- automatic_installation = false,
+                    ---@type string[]
+                    ensure_installed = servers,
+
+                    ---@type boolean | string[] | { exclude: string[] }
+                    automatic_enable = true,
+                  },
+                },
+                {
+                  "WhoIsSethDaniel/mason-tool-installer.nvim",
+                  enabled = true,
+                  opts = {
+                    ensure_installed = others,
+                  }
+                }
+              }
