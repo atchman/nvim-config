3869c47167a72b720340b588a9543d7212a1d677
atchman <atchman@users.noreply.github.com>
2025-07-06 22:20:21 +0200

    feat(plugin): table-mode & surraund
    both vim script plugins
    update lazy-lock

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,10 @@
+-- surround.vim
+--
+-- Delete/change/add 
+-- parentheses/quotes/XML-tags/much more with ease 
+
+return {
+  "tpope/vim-surround",
+  enabled = true,
+  version = "v2.x",
+}
---
+++ lua/plugins/table-mode.lua
@@ -0,0 +1,10 @@
+--  vim-table-mode
+--
+--  VIM Table Mode
+--  for instant table creation
+
+return {
+  "dhruvasagar/vim-table-mode",
+  enabled = true,
+  version = "v4.x.x",
+}
