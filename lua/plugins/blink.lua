de9efb493da86413cf56a7e104d562d4b3910146
atchman <atchman@users.noreply.github.com>
2025-07-21 00:08:33 +0200

    fix(blink): fix?

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,42 +2,44 @@
 --
 
 return {
-  {
-    "saghen/blink.cmp",
-    event = "VimEnter",
-    version = "1.*",
-    -- optional: provides snippets for the snippet source
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "folke/lazydev.nvim"
+  "saghen/blink.cmp",
+  event = "VimEnter",
+  version = "1.*",
+  -- optional: provides snippets for the snippet source
+  dependencies = {
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    "folke/lazydev.nvim",
+  },
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = "default" },
+
+    appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = "mono"
     },
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      keymap = { preset = "default" },
 
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono"
-      },
+    completion = { documentation = { auto_show = false } },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+    signature = { enabled = true },
 
-      snippets = { preset = 'luasnip' },
+    snippets = { preset = "luasnip" },
 
-      sources = {
-        -- add lazydev to your completion providers
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            -- make lazydev completions top priority (see `:h blink.cmp`)
-            score_offset = 100,
-          },
+    sources = {
+      -- add lazydev to your completion providers
+      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          -- make lazydev completions top priority (see `:h blink.cmp`)
+          score_offset = 100,
         },
       },
-      fuzzy = { implementation = 'lua' },
-
-      signature = { enabled = true },
     },
   },
+  opts_extend = { "sources.default" }
 }
+
